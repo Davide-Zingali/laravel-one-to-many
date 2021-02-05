8ff1c785a84e7e33f9af1e6e3a2e92f2f85d857c
Davide <kalelc351k@gmail.com>
2021-02-05 17:58:27 +0100

    aggiunta task base

--- project-laravel-one-to-many/database/migrations/2021_02_05_165601_create_tasks_table.php
+++ project-laravel-one-to-many/database/migrations/2021_02_05_165601_create_tasks_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateEmployeesTable extends Migration
+class CreateTasksTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,10 @@ class CreateEmployeesTable extends Migration
      */
     public function up()
     {
-        Schema::create('employees', function (Blueprint $table) {
+        Schema::create('tasks', function (Blueprint $table) {
             $table -> id();
 
-            $table -> string('name', 150);
-            $table -> string('lastname', 150);
-            $table -> date('dateOfBirth');
+            
 
             $table -> timestamps();
         });
@@ -31,6 +29,6 @@ class CreateEmployeesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('employees');
+        Schema::dropIfExists('tasks');
     }
 }
